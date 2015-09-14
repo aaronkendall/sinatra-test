b81dde8df43e2f9348ebd5208b4f9ffe15086249
Aaron <aarongkendall@gmail.com>
2015-09-14 16:11:47 +0100

    There is a cat now

--- hello.rb
+++ hello.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  'hello'
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
 
 get '/secret' do
-  'This is a secret page'
+  'This is a secret page and this is an addition to said secret page'
 end
